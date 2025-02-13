293d6ef0a764ce3c9ddb18f26626b6a7b888f377
Carla Chira <carlalexiachira@gmail.com>
2025-02-13 20:10:10 +0200

    finished program with comments added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    /**
+     * main function reads user input and executes changes
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path of .jpg photo to edit: ");
+        String imageFile = scanner.nextLine();
+        BufferedImage img = ImageIO.read(new File(imageFile));
+
+        Graphics2D g = img.createGraphics();
+        g.setColor(Color.BLACK);
+        System.out.println("Enter text to write: ");
+        String m = scanner.nextLine(); //text to write
+        int start = 0;
+        if(m.length() * 41 > img.getWidth()) {
+            System.out.println("The text is too long and won't fit entirely.");
+        }
+        else{
+            start = img.getWidth()/2 - m.length() / 2 * 41; //calculating point to start the text so that it's in the middle
+
+        }
+        char[] chars = new char[m.length()]; //text in array of chars
+        m.getChars(0, m.length() , chars, 0);
+
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 100));
+        g.drawChars(chars, 0, chars.length, start, img.getHeight()/2 );
+
+        g.dispose();
+
+        System.out.println("Enter path to edited .jpg photo: ");
+        String outputFile = scanner.nextLine();
+        try {
+            ImageIO.write(img, "jpg", new File(outputFile));
+            System.out.println("Editing complete.");
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
         }
     }
 }
